222124a0b06b9fe572796813775e35616d9eef36
Santi8kamppo <s.ocampor@uniandes.edu.co>
2021-11-04 07:09:32 -0500

    Add service adapter with initial required operations (#34)
    Revisado, contienen los dos endpoint que se requieren para el sprint1

--- Vinilos/app/src/main/java/com/example/vinilos/brokers/VolleyBroker.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/brokers/VolleyBroker.kt
@@ -13,7 +13,7 @@ class VolleyBroker constructor(context: Context) {
     val instance: RequestQueue = Volley.newRequestQueue(context.applicationContext)
 
     companion object{
-        const val BASE_URL= "https://vinyl-miso.herokuapp.com/"
+        private const val BASE_URL= "https://back-vinyls-populated.herokuapp.com/"
 
         fun getRequest(path:String, responseListener: Response.Listener<String>, errorListener: Response.ErrorListener): StringRequest {
             return StringRequest(Request.Method.GET, BASE_URL+path, responseListener,errorListener)
