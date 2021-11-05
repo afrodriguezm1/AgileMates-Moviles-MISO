88bbe9bda8cd21b178155ef89a9fe94d6f1768e6
Santi8kamppo <s.ocampor@uniandes.edu.co>
2021-11-04 20:51:02 -0500

    Add repository pattern, move broker logic into service adapter (#35)
    Revisado, contiene el repositorio y con la referencia al service Adapter

---
+++ Vinilos/app/src/main/java/com/example/vinilos/repositories/AlbumRepository.kt
@@ -0,0 +1,16 @@
+package com.example.vinilos.repositories
+
+import android.app.Application
+import com.android.volley.VolleyError
+import com.example.vinilos.models.Album
+import com.example.vinilos.network.NetworkServiceAdapter
+
+class AlbumRepository (private val application: Application){
+    fun refreshData(callback: (List<Album>)->Unit, onError: (VolleyError)->Unit) {
+        NetworkServiceAdapter.getInstance(application).getAlbums({
+            callback(it)
+        },
+            onError
+        )
+    }
+}
