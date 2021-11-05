a7b69939cd2fa0f2ab22f9a1633ad17d1b57b7f7
Andres Rodriguez <af.rodriguezm1@uniandes.edu.co>
2021-11-05 15:54:40 -0500

    Album cover added

--- Vinilos/app/src/main/java/com/example/vinilos/ui/home/AlbumAdapter.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/ui/home/AlbumAdapter.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.vinilos.R
 import com.example.vinilos.models.Album
+import com.squareup.picasso.Picasso
+import java.lang.Exception
 
 class AlbumAdapter(private val context: Activity, private val arraylist: ArrayList<Album>): ArrayAdapter<Album>(context, R.layout.album_item, arraylist) {
 
@@ -16,13 +19,25 @@ class AlbumAdapter(private val context: Activity, private val arraylist: ArrayLi
         val inflater: LayoutInflater = LayoutInflater.from(context)
         val view: View = inflater.inflate(R.layout.album_item, null)
 
+        val cover : ImageView = view.findViewById(R.id.albumCover)
         val name : TextView = view.findViewById(R.id.albumTitle)
         val year : TextView = view.findViewById(R.id.albumYear)
         val recordLabel : TextView = view.findViewById(R.id.albumSinger)
 
+
         name.text = arraylist[position].name
         year.text = arraylist[position].releaseDate
         recordLabel.text = arraylist[position].recordLabel
+        try{
+            Picasso.get()
+                .load(arraylist[position].cover)
+                .error(R.mipmap.ic_launcher_round)
+                .into(cover)
+        }
+        catch (e: Exception) {
+
+        }
+
 
         return view
     }
--- Vinilos/app/src/main/java/com/example/vinilos/ui/home/HomeFragment.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/ui/home/HomeFragment.kt
@@ -26,14 +26,14 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         albumList = ArrayList()
-        albumList.add(Album(1,"Bad", "Michael Jackson", "2000", "Album", "Pop", "Michael Jackson"))
-        albumList.add(Album(2,"Bad 2", "Michael Jackson", "2001", "Album", "Pop", "Michael Jackson"))
-        albumList.add(Album(3,"Bad 3", "Michael Jackson", "2002", "Album", "Pop", "Michael Jackson"))
-        albumList.add(Album(4,"Bad 4", "Michael Jackson", "2003", "Album", "Pop", "Michael Jackson"))
-        albumList.add(Album(5,"Bad 5", "Michael Jackson", "2004", "Album", "Pop", "Michael Jackson"))
-        albumList.add(Album(6,"Bad 6", "Michael Jackson", "2005", "Album", "Pop", "Michael Jackson"))
-        albumList.add(Album(7,"Bad 7", "Michael Jackson", "2006", "Album", "Pop", "Michael Jackson"))
-        albumList.add(Album(8,"Bad 8", "Michael Jackson", "2007", "Album", "Pop", "Michael Jackson"))
+        albumList.add(Album(1,"Bad", "https://m.media-amazon.com/images/I/6148jblbNqL._SL1500_.jpg", "1987", "Bad es el séptimo álbum de estudio del cantante estadounidense Michael Jackson,", "Pop", "Michael Jackson"))
+        albumList.add(Album(2,"Thriller", "https://m.media-amazon.com/images/I/71Y24VlCZGL._SL1500_.jpg", "1982", "Thriller es el sexto álbum de estudio del artista estadounidense Michael Jackson", "Pop", "Michael Jackson"))
+        albumList.add(Album(3,"Off the Wall", "https://m.media-amazon.com/images/I/81BXhwCtiKL._SL1500_.jpg", "1979", "Off the Wall es el quinto álbum de estudio solista de Michael Jackson editado en 1979", "Pop", "Michael Jackson"))
+        albumList.add(Album(4,"Dangerous", "https://img.europapress.es/fotoweb/fotonoticia_20161126095208_1200.jpg", "1991", "Dangerous es el octavo álbum de estudio del cantante estadounidense Michael Jackson,", "Pop", "Michael Jackson"))
+        albumList.add(Album(5,"Ben", "https://m.media-amazon.com/images/I/51FxxrmpPKL.jpg", "1972", "Ben es el segundo álbum de Michael Jackson a los 13 años de edad,", "Pop", "Michael Jackson"))
+        albumList.add(Album(6,"Invincible", "https://m.media-amazon.com/images/I/71TXrGXF8TL._SL1500_.jpg", "2001", "nvincible es el décimo y último álbum de estudio del cantante estadounidense Michael Jackson", "Pop", "Michael Jackson"))
+        albumList.add(Album(7,"Got to Be There", "https://m.media-amazon.com/images/I/51Zyd1d+SmL.jpg", "1972", "Got to Be There es el primer álbum de estudio como solista con el que Michael Jackson debutó en 1972,", "Pop", "Michael Jackson"))
+        albumList.add(Album(8,"Bad 8", "", "2007", "Album", "Pop", "Michael Jackson"))
 
         binding.lvAlbum.adapter = getActivity()?.let { AlbumAdapter(it, albumList) }
 
