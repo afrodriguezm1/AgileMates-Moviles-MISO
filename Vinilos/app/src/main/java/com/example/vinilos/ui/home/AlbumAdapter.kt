625a54f18ca058e2e4ddcc776e7371a18860a40c
Andres Rodriguez <af.rodriguezm1@uniandes.edu.co>
2021-11-05 22:52:00 -0500

    Conection

--- Vinilos/app/src/main/java/com/example/vinilos/ui/home/AlbumAdapter.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/ui/home/AlbumAdapter.kt
@@ -4,39 +4,47 @@ import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.example.vinilos.R
+import com.example.vinilos.databinding.AlbumItemBinding
 import com.example.vinilos.models.Album
 import com.squareup.picasso.Picasso
 import java.lang.Exception
 
-class AlbumAdapter(private val context: Activity, private val arraylist: ArrayList<Album>): ArrayAdapter<Album>(context, R.layout.album_item, arraylist) {
+class AlbumsAdapter : RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>(){
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-        val inflater: LayoutInflater = LayoutInflater.from(context)
-        val view: View = inflater.inflate(R.layout.album_item, null)
-
-        val cover : ImageView = view.findViewById(R.id.albumCover)
-        val name : TextView = view.findViewById(R.id.albumTitle)
-        val year : TextView = view.findViewById(R.id.albumYear)
-        val recordLabel : TextView = view.findViewById(R.id.albumSinger)
-
-
-        name.text = arraylist[position].name
-        year.text = arraylist[position].releaseDate
-        recordLabel.text = arraylist[position].recordLabel
-        try{
-            Picasso.get()
-                .load(arraylist[position].cover)
-                .error(R.mipmap.ic_launcher_round)
-                .into(cover)
+    var albums :List<Album> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
         }
-        catch (e: Exception) {}
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
+        val withDataBinding: AlbumItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            AlbumViewHolder.LAYOUT,
+            parent,
+            false)
+        return AlbumViewHolder(withDataBinding)
+    }
 
-        return view
+    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.album = albums[position]
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return albums.size
+    }
+
+    class AlbumViewHolder(val viewDataBinding: AlbumItemBinding) :
+        RecyclerView.ViewHolder(viewDataBinding.root) {
+        companion object {
+            @LayoutRes
+            val LAYOUT = R.layout.album_item
+        }
     }
 }
--- Vinilos/app/src/main/java/com/example/vinilos/ui/home/HomeFragment.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/ui/home/HomeFragment.kt
@@ -5,42 +5,62 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vinilos.R
 
 import com.example.vinilos.databinding.FragmentHomeBinding
 import com.example.vinilos.models.Album
 import com.example.vinilos.viewmodels.AlbumViewModel
 
 class HomeFragment : Fragment() {
-
     private var _binding: FragmentHomeBinding? = null
-
-    private lateinit var albumViewMode: AlbumViewModel
-
-    private lateinit var albumList : ArrayList<Album>
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewModel: AlbumViewModel
+    private var viewModelAdapter: AlbumsAdapter? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val view = binding.root
+        viewModelAdapter = AlbumsAdapter()
+        return view
+    }
 
-        albumList = ArrayList()
-        albumList.add(Album(1,"Bad", "https://m.media-amazon.com/images/I/6148jblbNqL._SL1500_.jpg", "1987", "Bad es el séptimo álbum de estudio del cantante estadounidense Michael Jackson,", "Pop", "Michael Jackson"))
-        albumList.add(Album(2,"Thriller", "https://m.media-amazon.com/images/I/71Y24VlCZGL._SL1500_.jpg", "1982", "Thriller es el sexto álbum de estudio del artista estadounidense Michael Jackson", "Pop", "Michael Jackson"))
-        albumList.add(Album(3,"Off the Wall", "https://m.media-amazon.com/images/I/81BXhwCtiKL._SL1500_.jpg", "1979", "Off the Wall es el quinto álbum de estudio solista de Michael Jackson editado en 1979", "Pop", "Michael Jackson"))
-        albumList.add(Album(4,"Dangerous", "https://img.europapress.es/fotoweb/fotonoticia_20161126095208_1200.jpg", "1991", "Dangerous es el octavo álbum de estudio del cantante estadounidense Michael Jackson,", "Pop", "Michael Jackson"))
-        albumList.add(Album(5,"Ben", "https://m.media-amazon.com/images/I/51FxxrmpPKL.jpg", "1972", "Ben es el segundo álbum de Michael Jackson a los 13 años de edad,", "Pop", "Michael Jackson"))
-        albumList.add(Album(6,"Invincible", "https://m.media-amazon.com/images/I/71TXrGXF8TL._SL1500_.jpg", "2001", "nvincible es el décimo y último álbum de estudio del cantante estadounidense Michael Jackson", "Pop", "Michael Jackson"))
-        albumList.add(Album(7,"Got to Be There", "https://m.media-amazon.com/images/I/51Zyd1d+SmL.jpg", "1972", "Got to Be There es el primer álbum de estudio como solista con el que Michael Jackson debutó en 1972,", "Pop", "Michael Jackson"))
-        albumList.add(Album(8,"Bad 8", "", "2007", "Album", "Pop", "Michael Jackson"))
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.albumsRv
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = viewModelAdapter
+    }
 
-        binding.lvAlbum.adapter = getActivity()?.let { AlbumAdapter(it, albumList) }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onActivityCreated()"
+        }
 
-        return root
+        viewModel = ViewModelProvider(
+            this,
+            AlbumViewModel.Factory(activity.application)
+        ).get(AlbumViewModel::class.java)
+        viewModel.albums.observe(viewLifecycleOwner, Observer<List<Album>> {
+            it.apply {
+                viewModelAdapter!!.albums = this
+            }
+        })
+        viewModel.eventNetworkError.observe(
+            viewLifecycleOwner,
+            Observer<Boolean> { isNetworkError ->
+                if (isNetworkError) onNetworkError()
+            })
     }
 
     override fun onDestroyView() {
@@ -48,9 +68,10 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
+    private fun onNetworkError() {
+        if (!viewModel.isNetworkErrorShown.value!!) {
+            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            viewModel.onNetworkErrorShown()
+        }
     }
 }
-
