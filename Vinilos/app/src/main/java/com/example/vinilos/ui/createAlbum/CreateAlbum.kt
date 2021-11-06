e8a78dd5a8f4e059cbc166de0a76b79cc24e2a1c
Fabian Batioja <fabian.batioja@openlms.net>
2021-11-06 12:32:57 -0500

    Cerate album form

--- Vinilos/app/src/main/java/com/example/vinilos/ui/createAlbum/CreateAlbum.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/ui/createAlbum/CreateAlbum.kt
@@ -2,6 +2,11 @@ package com.example.vinilos.ui.createAlbum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.vinilos.R
 
 class CreateAlbum : AppCompatActivity() {
@@ -13,10 +18,40 @@ class CreateAlbum : AppCompatActivity() {
         //set actionbar title
         actionbar!!.title = getString(R.string.activity_create_album_tittle);
         actionbar.setDisplayHomeAsUpEnabled(true)
+
+        val spinnerGenero: Spinner = findViewById(R.id.spinnerAlbumGenero)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.opciones_genero,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerGenero.adapter = adapter
+        }
+
+        val spinnerDisquera: Spinner = findViewById(R.id.spinnerAlbumDisquera)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.opciones_disquera,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerDisquera.adapter = adapter
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    fun cancelCreation(view: View) {
+        this.onBackPressed();
     }
 }
