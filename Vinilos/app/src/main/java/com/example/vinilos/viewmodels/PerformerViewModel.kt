2196a473c983d4fde9c8f092e93712b116b013c9
Bony Casas <b.casas@uniandes.edu.co>
2021-11-10 07:56:30 -0500

    Model Performer

---
+++ Vinilos/app/src/main/java/com/example/vinilos/viewmodels/PerformerViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.vinilos.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class PerformerViewModel (application: Application) :  AndroidViewModel(application) {
+    //TODO: Implementation
+}
