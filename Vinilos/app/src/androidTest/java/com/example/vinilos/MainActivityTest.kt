147c8cc30fb0c168e16f050b4683a7d4db1c27c8
Bony Casas <b.casas@uniandes.edu.co>
2021-11-11 06:23:55 -0500

    Test Album list

--- Vinilos/app/src/androidTest/java/com/example/vinilos/MainActivityTest.kt
+++ Vinilos/app/src/androidTest/java/com/example/vinilos/MainActivityTest.kt
@@ -1,5 +1,6 @@
 package com.example.vinilos
 
+import android.view.View
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.launchActivity
@@ -8,13 +9,21 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.vinilos.MainActivity
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.recyclerview.widget.RecyclerView
+
+import androidx.test.espresso.matcher.BoundedMatcher
+
+import androidx.annotation.NonNull
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -33,12 +42,36 @@ class MainActivityTest{
         //Validar que hay al menos un registro con los datos completos
         //Prerrequesitos: Se asume que existe al menos un registro en la lista de albums
 
-        //El album tiene cargada una imagen y con nombre
-        onView(withId(R.id.imageCover)).check(matches(ViewMatchers.isDisplayed()))
-        onView(withId(R.id.textView1)).check(matches(ViewMatchers.isDisplayed()))
+        Thread.sleep(5000);
+
+        //Valida si la lista de albumes esta renderizada
+        onView(withId(R.id.albumsRv)).check(matches(ViewMatchers.isDisplayed()))
+
+        //Valida si el primer item de la lista esta renderizado y se muestra el nombre.
+        // este segundo test debe estar en otra funcion con el @test
+        onView(withId(R.id.albumsRv)).check(matches(atPosition(0, hasDescendant(withText("Buscando América")))))
 
         //Espresso.closeSoftKeyboard()
         //onView(withId(R.id.button2)).perform(click())
 
     }
+    //TODO: Enviar a una clase utilitaria porque esta la podemos usar en todos los test.
+    // La tome de starkoverflow
+    fun atPosition(position: Int, itemMatcher: Matcher<View?>): Matcher<View?>? {
+        checkNotNull(itemMatcher)
+        return object : BoundedMatcher<View?, RecyclerView>(RecyclerView::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
+            }
+
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val viewHolder = view.findViewHolderForAdapterPosition(position)
+                    ?: // has no item on such position
+                    return false
+                return itemMatcher.matches(viewHolder.itemView)
+            }
+        }
+    }
 }
+
