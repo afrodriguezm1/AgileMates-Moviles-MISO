802f65e894c55db17f472cacccb81f08049a29d7
Carolina Casas <b.casas@uniandes.edu.co>
2021-11-17 09:01:01 -0500

    Ft performer detail (#58)
    * update performer to support band and musician
    
    * Add performer type to customer
    
    * back to detail performer
    
    * Performer Detail
    
    * Performer Detail
    
    * Performer Detail
    
    * Performer Detail
    
    * Prevent duplicate album from detail performer

--- Vinilos/app/src/main/java/com/example/vinilos/ui/performerDetail/PerformerDetail.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/ui/performerDetail/PerformerDetail.kt
@@ -1,33 +1,17 @@
 package com.example.vinilos.ui.performerDetail
 
-import android.R.attr
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.vinilos.R
-import com.example.vinilos.models.Album
 import com.example.vinilos.models.Performer
-import com.example.vinilos.models.PerformerType
 import com.example.vinilos.ui.artistas.PerformerAdapter
 import com.example.vinilos.viewmodels.PerformerViewModel
 import com.squareup.picasso.Picasso
-import android.view.ViewGroup
-import androidx.core.view.marginRight
-import android.R.attr.right
-
-import android.R.attr.left
-
-
-
 
 
 class PerformerDetail : AppCompatActivity() {
@@ -72,15 +56,20 @@ class PerformerDetail : AppCompatActivity() {
                 val scale = resources.displayMetrics.density
                 val dpWidthInPx = (200 * scale).toInt()
 
-                performer?.albums?.forEach {
-                    val imageView = ImageView(albumPerformerImg.context)
-                    imageView.layoutParams= LinearLayout.LayoutParams(dpWidthInPx, dpWidthInPx)
-                    val lp = LinearLayout.LayoutParams(dpWidthInPx, dpWidthInPx)
-                    lp.setMargins(0, 0,50,0)
-                    imageView.setLayoutParams(lp);
-                    val layout = findViewById<LinearLayout>(R.id.containerPerformerAlbumList)
-                    layout?.addView(imageView)
-                    loadImage(it.cover,imageView)
+                val layout = findViewById<LinearLayout>(R.id.containerPerformerAlbumList)
+                val countAlbumImageView = layout.childCount
+
+                if(countAlbumImageView == 1) {
+                    performer?.albums?.forEach {
+                        val imageView = ImageView(albumPerformerImg.context)
+                        imageView.layoutParams = LinearLayout.LayoutParams(dpWidthInPx, dpWidthInPx)
+                        val lp = LinearLayout.LayoutParams(dpWidthInPx, dpWidthInPx)
+                        lp.setMargins(0, 0, 50, 0)
+                        imageView.setLayoutParams(lp);
+                        val layout = findViewById<LinearLayout>(R.id.containerPerformerAlbumList)
+                        layout?.addView(imageView)
+                        loadImage(it.cover, imageView)
+                    }
                 }
             }
         })
