b96e032a2393602e6121b12f5889e81e20ca6aa2
Andrés Rodriguez <af.rodriguezm1@uniandes.edu.co>
2021-11-19 19:29:41 -0500

    Loading cache (#63)
    * Save and read albums on cache
    
    * Save and read performers on cache

---
+++ Vinilos/app/src/main/java/com/example/vinilos/network/CacheManager.kt
@@ -0,0 +1,90 @@
+package com.example.vinilos.network
+
+import android.content.Context
+import android.util.Log
+import com.example.vinilos.models.Album
+import com.example.vinilos.models.Performer
+import com.example.vinilos.models.PerformerType
+
+class CacheManager (context: Context) {
+    companion object{
+        var instance: CacheManager? = null
+        fun getInstance(context: Context)=
+            instance ?: synchronized(this) {
+                instance ?: CacheManager(context).also {
+                    instance = it
+                }
+            }
+    }
+    private var albums: HashMap<String, List<Album>> = hashMapOf<String, List<Album>>()
+
+    private val performers : HashMap<String, List<Performer>> = hashMapOf<String, List<Performer>>()
+
+    fun addAlbums( album: List<Album>) {
+        albums["albumes"] = album
+    }
+
+    fun addAlbum(album: Album){
+        if(albums.containsKey("albumes")){
+            val albumes = albums["albumes"]!!.toMutableList()
+            for(i in 0 until albumes.size){
+                if(albumes[i].id == album.id){
+                    albumes[i] = album
+                    albums["albumes"] = albumes
+                    break
+                }
+            }
+        }
+    }
+
+    fun addPerformers(performer: List<Performer>){
+        performers["performers"] = performer
+    }
+
+    fun addPerformer(performer: Performer){
+        if(performers.containsKey("performers")){
+            val perfor = performers["performers"]!!.toMutableList()
+            for(i in 0 until perfor.size){
+                if(perfor[i].id == performer.id){
+                    perfor[i] = performer
+                    performers["performers"] = perfor
+                    break
+                }
+            }
+        }
+    }
+
+    fun getAlbums(): List<Album> {
+        return if (albums.containsKey("albumes")) albums["albumes"]!! else listOf<Album>()
+    }
+
+    fun getAlbum(albumId: Int): Album {
+        if(albums.containsKey("albumes")){
+            val albumes: List<Album> = albums["albumes"]!!
+            for(i in 0 until albumes.size){
+                if(albumes[i].id == albumId){
+                    return albumes[i]
+                }
+            }
+        }
+        return Album(id = 0, name = "", cover = "", releaseDate = "", description = "", genre = "",recordLabel = "", tracks = emptyList(), performers = emptyList())
+    }
+
+    fun getPerformers(): List<Performer>{
+        return if (performers.containsKey("performers")) performers["performers"]!! else listOf<Performer>()
+    }
+
+    fun getPerformer(performerType: PerformerType, performerId: Int): Performer{
+        if(performers.containsKey("performers")){
+            val performers: List<Performer> = performers["performers"]!!
+            for(i in 0 until performers.size){
+                if(performers[i].performerId == performerId && performers[i].performerType == performerType){
+                    return performers[i]
+                }
+            }
+        }
+        return Performer(
+            id = "", performerId = 0 , name = "", image = "", description = "", date = "", performerType = PerformerType.MUSICIAN, albums = listOf<Album>()
+        )
+    }
+}
--- Vinilos/app/src/main/java/com/example/vinilos/repositories/PerformerRepository.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/repositories/PerformerRepository.kt
@@ -3,21 +3,35 @@ package com.example.vinilos.repositories
 import android.app.Application
 import com.example.vinilos.models.Performer
 import com.example.vinilos.models.PerformerType
+import com.example.vinilos.network.CacheManager
 import com.example.vinilos.network.NetworkServiceAdapter
 
 class PerformerRepository (private val application: Application){
 
     suspend fun refreshPerformerList(): List<Performer> {
-        val performers = NetworkServiceAdapter.getInstance(application).getMusicians()
-        val bands = NetworkServiceAdapter.getInstance(application).getBands()
-        return performers.plus(bands)
-
+        try {
+            val performers = NetworkServiceAdapter.getInstance(application).getMusicians()
+            val bands = NetworkServiceAdapter.getInstance(application).getBands()
+            CacheManager.getInstance(application.applicationContext).addPerformers(performers.plus(bands))
+            return performers.plus(bands)
+        }catch (e: Exception) {
+            return CacheManager.getInstance(application.applicationContext).getPerformers()
+        }
     }
 
     suspend fun refreshPerformerDetail(performerType: PerformerType, performerId: Int): Performer {
-        if(performerType == PerformerType.BAND){
-            return NetworkServiceAdapter.getInstance(application).getBandsDetail(performerId)
+        try {
+            var performer : Performer
+            if(performerType == PerformerType.BAND){
+                performer = NetworkServiceAdapter.getInstance(application).getBandsDetail(performerId)
+            }
+            else {
+                performer = NetworkServiceAdapter.getInstance(application).getMusicianDetail(performerId)
+            }
+            CacheManager.getInstance(application.applicationContext).addPerformer(performer)
+            return performer
+        }catch (e: Exception) {
+            return CacheManager.getInstance(application.applicationContext).getPerformer(performerType, performerId)
         }
-        return NetworkServiceAdapter.getInstance(application).getMusicianDetail(performerId)
     }
 }
