0fb71a9154e47339235ac0611f6e009d64c3c4e5
Bony Casas <b.casas@uniandes.edu.co>
2021-11-23 21:50:04 -0500

    Collector list

---
+++ Vinilos/app/src/main/java/com/example/vinilos/repositories/CollectorRepository.kt
@@ -0,0 +1,20 @@
+package com.example.vinilos.repositories
+
+import android.app.Application
+import com.example.vinilos.models.Collector
+import com.example.vinilos.network.CacheManager
+import com.example.vinilos.network.NetworkServiceAdapter
+
+class CollectorRepository (private val application: Application){
+
+    suspend fun refreshCollectorList(): List<Collector> {
+        var resp = emptyList<Collector>()
+        try{
+            resp = NetworkServiceAdapter.getInstance(application).getCollectors()
+            CacheManager.getInstance(application.applicationContext).addCollectors(resp)
+        }catch (e: Exception) {
+            resp = CacheManager.getInstance(application.applicationContext).getCollectors()
+        }
+        return resp
+    }
+}
